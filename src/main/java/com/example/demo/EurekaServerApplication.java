de325db85a0eb2451022e0cd0e16f5c03fbd3431
painAdmin <1767123925@qq.com>
2018-03-12 14:39:21 +0800

    增加类注释

--- src/main/java/com/example/demo/EurekaServerApplication.java
+++ src/main/java/com/example/demo/EurekaServerApplication.java
@@ -3,6 +3,12 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+/**
+ * eureka 服务注册中心  服务端
+ * @author pain
+ *
+ */
+
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerApplication {
